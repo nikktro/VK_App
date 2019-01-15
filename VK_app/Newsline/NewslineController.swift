696be9e037df2fb579f6089d99eefe132c5cb9ef
Nikolay Trofimov <nikktro@gmail.com>
2019-01-15 12:01:19 +0300

    Add Newsline VC, Like Control

---
+++ VK_app/Newsline/NewslineController.swift
@@ -0,0 +1,30 @@
+//
+//  NewslineController.swift
+//  VK_app
+//
+//  Created by Nikolay.Trofimov on 15/01/2019.
+//  Copyright © 2019 Nikolay.Trofimov. All rights reserved.
+//
+
+import UIKit
+
+class NewslineController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
