eee2b5b1ecc3e8a1825c9b59a0e9cc93e30f4b46
Nikolay Trofimov <nikktro@gmail.com>
2019-01-20 01:42:00 +0300

    Generation of Chars and User List for Users. Added section header and user sort

--- VK_app/UserFriendList/UserFriendController.swift
+++ VK_app/UserFriendList/UserFriendController.swift
@@ -30,38 +30,66 @@ class UserFriendController: UITableViewController {
       Friend(name: "Дмитрий Нестеренко", image: UIImage(named: "userDmitry"))
     ]
   
+  
+  var userDictionary = [String: [String]]() // Буква и список пользователей
+  var userSectionTitles = [String]() // Буква пользователя
+  
     override func viewDidLoad() {
         super.viewDidLoad()
+      
+      // Генерация Букв и Списка пользователей
+      for human in friendList {
+        let userKey = String(human.name.prefix(1))
+        if var humanValues = userDictionary[userKey] {
+          humanValues.append(human.name)
+          userDictionary[userKey] = humanValues
+        } else {
+          userDictionary[userKey] = [human.name]
+        }
+      }
+      // сортировака букв пользователей
+      userSectionTitles = [String](userDictionary.keys)
+      userSectionTitles.sort(by: { $0 < $1 })
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
 
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return userSectionTitles.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return friendList.count
+    
+      let userKey = userSectionTitles[section]
+      if let userValues = userDictionary[userKey] {
+        return userValues.count
+      }
+      
+      return 0
+      
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
       // получаем ячейку из пула
       let cell = tableView.dequeueReusableCell(withIdentifier: "UserFriends", for: indexPath) as! UserFriendCell
-      // получаем друга
-      let friend = friendList[indexPath.row]
-      // устанавливаем друга в надпись и его аватарку
-      cell.userFriendLabel.text = friend.name
-      cell.userFriendImage.image = friend.image
-      
+    
+      // устанавливаем друга в надпись
+      let userKey = userSectionTitles[indexPath.section]
+      if let userValues = userDictionary[userKey] {
+        cell.userFriendLabel.text = userValues[indexPath.row]
+        
+        // ищем аватарку по имени
+        for item in friendList {
+          if item.name == userValues[indexPath.row] {
+            cell.userFriendImage.image = item.image
+          }
+        }
+      }
       return cell
     }
 
@@ -79,50 +107,15 @@ class UserFriendController: UITableViewController {
       
     }
   }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
+  // Буквы быстрого перехода
+  override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+    return userSectionTitles
+  }
+  
+  // Буквы заголовка
+  override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    return String(userSectionTitles[section])
+  }
 
 }
