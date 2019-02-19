9f9ffd5e12e51f4b204d1f21ac1b962ddfa074ab
Nikolay Trofimov <nikktro@gmail.com>
2019-02-20 01:43:50 +0300

    add SearchBar for Friends

--- VK_app/UserFriendList/UserFriendController.swift
+++ VK_app/UserFriendList/UserFriendController.swift
@@ -14,9 +14,13 @@ class UserFriendController: UITableViewController, UISearchBarDelegate {
   @IBOutlet weak var searchBar: UISearchBar!
   
   var friendList = [Friend]()
+  var searchFriendList = [Friend]() // Массив для поиска
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    // for SearchBar
+    searchBar.delegate = self
     
     // VKService User FriendList
     let vkService = VKService()
@@ -32,6 +36,7 @@ class UserFriendController: UITableViewController, UISearchBarDelegate {
         // сортировка и обновление интерфейса в главном потоке
         DispatchQueue.main.async {
           self.friendList = self.friendList.sorted()
+          self.searchFriendList = self.friendList
           self.tableView.reloadData()
         }
       }
@@ -42,14 +47,14 @@ class UserFriendController: UITableViewController, UISearchBarDelegate {
   
   // numberOfRowsInSection
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return friendList.count
+    return searchFriendList.count
   }
   
   
   // dequeueReusableCell
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "UserFriends", for: indexPath) as! UserFriendCell
-    cell.configure(with: friendList[indexPath.row])
+    cell.configure(with: searchFriendList[indexPath.row])
     return cell
   }
   
@@ -67,8 +72,29 @@ extension UserFriendController {
       // определяем выбранного пользователя
       guard let indexPath = userFriendController.tableView.indexPathForSelectedRow else { return }
       // отправляем Friend Id в коллекцию friendImageController
-      friendImageController.selectedFriendId = friendList[indexPath.row].id
+      friendImageController.selectedFriendId = searchFriendList[indexPath.row].id
       
     }
+  }
+}
+
+extension UserFriendController {
+  // SearchBar
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    
+    // очищаем фильтр, если запрос пустой
+    guard !searchText.isEmpty else {
+      searchFriendList = friendList
+      tableView.reloadData()
+      return
+    }
+    
+    // фильтр по lowercase в имени и фамилии
+    searchFriendList = friendList.filter({ user -> Bool in
+      //let namesurname = (user.name.lowercased() + user.surname.lowercased())
+      return (user.first_name.lowercased() + user.last_name.lowercased()).contains(searchText.lowercased())
+    })
+    tableView.reloadData()
+    
   }
 }
