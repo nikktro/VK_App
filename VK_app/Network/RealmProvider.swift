fe95c486e3e27f145ed8144b8d5f95f313f90281
Nikolay Trofimov <nikktro@gmail.com>
2020-09-22 22:10:47 +0300

    project update
    Update pods, project to 5.3

--- VK_app/Network/RealmProvider.swift
+++ VK_app/Network/RealmProvider.swift
@@ -17,7 +17,7 @@ class RealmProvider {
     do {
       let realm = try Realm(configuration: config)
       try realm.write {
-        realm.add(items, update: update)
+        realm.add(items, update: .all)
       }
       
     } catch {
